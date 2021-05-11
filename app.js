d37008182c74f2c562602560388873a416942ada
Yue.Zhao01 <Yue.Zhao01@geely.com>
2021-05-11 14:39:27 +0800

    添加express模式，支持代理接口

---
+++ app.js
@@ -0,0 +1,23 @@
+// 如果有必要使用Express，比如纯静态网页部分接口调试需要跨域代理等
+
+const express = require('express')
+const proxy = require('http-proxy-middleware')
+
+const file_path = process.argv[2] || 'src'
+
+const app = express()
+
+app.use(express.static(file_path))
+
+app.use(
+  '/api/test_proxy',
+  proxy.createProxyMiddleware({
+    target: 'https://www.baidu.com',
+    changeOrigin: true,
+    pathRewrite: {
+      '^/api/test_proxy': '/',
+    },
+  })
+)
+
+app.listen(8000)
--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,14 @@
 console.log(1)
 
 function test() {
-  console.log('test')
+  console.log('test run')
+  fetch('/api/test_proxy')
+    .then((ret) => {
+      return ret.text()
+    })
+    .then((text) => {
+      console.log(text)
+    })
 }
 
 test()
