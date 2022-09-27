f4dd02316f2f0a33c1d37a7c5e8d7196beeb6241
kvker <kk@kvker.com>
2022-09-27 21:48:57 +0800

    feat 添加打包错误跳过

--- views/build.js
+++ views/build.js
@@ -2,6 +2,7 @@ const fs = require('fs')
 const exec = require('child_process').exec
 
 fs.readdir('./', { encoding: 'utf-8'}, (error, file_list) => {
+  if(error) return
   let ejs_list = file_list.filter(file => file.endsWith('.ejs'))
   ejs_list.forEach(ejs => {
     exec(`ejs ${ejs} -o ../public/${ejs.replace('.ejs', '')}.html`, (error, ret) => {
